2750bb3e264f370adf30ec37f30a3076052ea3af
Christopher Lewis <chris.lewis@hudl.com>
2015-09-16 19:27:49 +0100

    Got everything done just about

--- RWCExchange/Exchange.cs
+++ RWCExchange/Exchange.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RWCExchange
 {
@@ -8,9 +9,10 @@ namespace RWCExchange
         public Bid Bid { get; set; }
         public Ask Ask { get; set; }
         public double Price { get; set; }
+        public bool Update { get; set; }
     }
 
-    public class Bid : IComparable<Bid>
+    public class Bid : IComparable<Bid>, IEquatable<Bid>
     {
         public DateTime TimeStamp { get; set; }
         public string User { get; set; }
@@ -23,9 +25,36 @@ namespace RWCExchange
             if (TimeStamp < other.TimeStamp) return -1;
             return TimeStamp > other.TimeStamp ? 1 : 0;
         }
+
+        public bool Equals(Bid other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return TimeStamp.Equals(other.TimeStamp) && string.Equals(User, other.User) && Price.Equals(other.Price);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Bid)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = TimeStamp.GetHashCode();
+                hashCode = (hashCode * 397) ^ (User != null ? User.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ Price.GetHashCode();
+                return hashCode;
+            }
+        }
+
     }
 
-    public class Ask : IComparable<Ask>
+    public class Ask : IComparable<Ask>, IEquatable<Ask>
     {
         public DateTime TimeStamp { get; set; }
         public string User { get; set; }
@@ -38,23 +67,46 @@ namespace RWCExchange
             if (TimeStamp < other.TimeStamp) return -1;
             return TimeStamp > other.TimeStamp ? 1 : 0;
         }
+
+        public bool Equals(Ask other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return TimeStamp.Equals(other.TimeStamp) && string.Equals(User, other.User) && Price.Equals(other.Price);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Ask) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = TimeStamp.GetHashCode();
+                hashCode = (hashCode*397) ^ (User != null ? User.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ Price.GetHashCode();
+                return hashCode;
+            }
+        }
     }
 
     public sealed class Exchange
     {
         private static readonly Lazy<Exchange> _instance = new Lazy<Exchange>(()=>new Exchange());
 
-        private Dictionary<string, List<Bid>> _bids;
-        private Dictionary<string, List<Ask>> _asks;
+        private readonly Dictionary<string, List<Bid>> _bids;
+        private readonly Dictionary<string, List<Ask>> _asks;
 
-        private Dictionary<string, string> _owners; 
+        private readonly Dictionary<string, string> _owners; 
 
         private readonly List<string> _countries; 
 
-        public static Exchange Instance
-        {
-            get { return _instance.Value; }
-        }
+        public static Exchange Instance => _instance.Value;
 
         private Exchange()
         {
@@ -75,48 +127,160 @@ namespace RWCExchange
             return _countries.Contains(country);
         }
 
+        public bool CountryDropped(string country)
+        {
+            return !_owners.ContainsKey(country);
+        }
+
         public bool IsCurrentOwner(string country, string user)
         {
             return _owners[country] == user;
         }
 
+        public List<Bid> GetBids(string country)
+        {
+            return _bids[country].ToList();
+        }
+
+        public List<Ask> GetAsks(string country)
+        {
+            return _asks[country].ToList();
+        }
+
+        public List<KeyValuePair<string, string>> GetOwners()
+        {
+            return _owners.ToList();
+        } 
+
         public Trade AddBid(string country,Bid bid)
         {
-            var lowestAsk = _asks[country][0];
-            if (bid.Price >= lowestAsk.Price && lowestAsk.User != bid.User)
+            var existingForUser = _bids[country].FirstOrDefault(i => i.User == bid.User);
+            var trade = new Trade();
+            if (existingForUser != null)
             {
-                _asks[country].Remove(lowestAsk);
-                _owners[country] = bid.User;
-                return new Trade {Bid = bid, Ask = lowestAsk, Price = bid.Price};
+                existingForUser.TimeStamp = bid.TimeStamp;
+                existingForUser.Price = bid.Price;
+                trade.Update = true;
+                _bids[country].Sort();
+                if (!_asks[country].Any() || !_bids[country][0].Equals(bid))
+                {
+                    return trade;
+                }
             }
-            _bids[country].Add(bid);
-            _bids[country].Sort();
-            return null;
+            if (!_asks[country].Any() && !trade.Update)
+            {
+                _bids[country].Add(bid);
+                _bids[country].Sort();
+                return trade;
+            }
+            var lowestAsk = _asks[country][0];
+            if (bid.Price >= lowestAsk.Price)
+            {
+                _asks[country].Clear();
+                _owners[country] = bid.User;
+                trade.Bid = bid;
+                trade.Ask = lowestAsk;
+                trade.Price = bid.Price;
+                trade.Update = false;
+                return trade;
+            }
+            if (!trade.Update)
+            {
+                _bids[country].Add(bid);
+                _bids[country].Sort();
+            }
+            return trade;
         }
 
         public Trade AddAsk(string country, Ask ask)
         {
+            var existingForUser = _asks[country].FirstOrDefault(i => i.User == ask.User);
+            var trade = new Trade();
+            if (existingForUser != null)
+            {
+                existingForUser.TimeStamp = ask.TimeStamp;
+                existingForUser.Price = ask.Price;
+                trade.Update = true;
+                if (!_bids[country].Any())
+                {
+                    return trade;
+                }
+            }
+            if (!_bids[country].Any() && !trade.Update)
+            {
+                _asks[country].Add(ask);
+                return trade;
+            }
             var highestBid = _bids[country][0];
-            if (highestBid.Price >= ask.Price && ask.User != highestBid.User)
+            if (highestBid.Price >= ask.Price)
             {
                 _bids[country].Remove(highestBid);
-                _owners[country] = ask.User;
-                return new Trade { Bid = highestBid, Ask = ask, Price = ask.Price };
+                _owners[country] = highestBid.User;
+                trade.Bid = highestBid;
+                trade.Ask = ask;
+                trade.Price = ask.Price;
+                trade.Update = false;
+                return trade;
             }
-            _asks[country].Add(ask);
-            _asks[country].Sort();
-            return null;
+            if (!trade.Update)
+            {
+                _asks[country].Add(ask);
+            }
+            return trade;
         }
 
         public Bid GetBestBid(string country)
         {
-            return _bids[country][0];
+            return !_bids[country].Any()?null:_bids[country][0];
         }
 
         public Ask GetBestAsk(string country)
         {
-            return _asks[country][0];
+            return !_asks[country].Any()?null:_asks[country][0];
         }
+
+        public string DropTeam(string country)
+        {
+            _bids[country].Clear();
+            _asks[country].Clear();
+            var current = _owners[country];
+            _owners.Remove(country);
+            return current;
+        }
+
+        public bool CurrentlyOwned(string country)
+        {
+            return _owners[country] != string.Empty;
+        }
+
+        public bool PullBid(string country, string user)
+        {
+            var bid = _bids[country].FirstOrDefault(i => i.User == user);
+            if (bid == null) return false;
+            _bids[country].Remove(bid);
+            _bids[country].Sort();
+            return true;
+        }
+
+        public bool PullAsk(string country, string user)
+        {
+            var ask = _asks[country].FirstOrDefault(i => i.User == user);
+            if (ask == null) return false;
+            _asks[country].Remove(ask);
+            return true;
+        }
+
+        public bool SetOwner(string country, string user)
+        {
+            _owners[country] = user;
+            if (user == "house")
+            {
+                _asks[country].Clear();
+                _asks[country].Add(new Ask() {Price = 0.01,TimeStamp = DateTime.Now,User = "house"});
+            }
+            return true;
+        }
+
 
 
     }
--- RWCExchange/SlackWebHookHandler.cs
+++ RWCExchange/SlackWebHookHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Specialized;
+using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNet.WebHooks;
@@ -9,8 +11,11 @@ namespace RWCExchange
 {
     public class SlackWebHookHandler : WebHookHandler
     {
-        private readonly Regex _regex = new Regex(@"^\:(?<bs>buy|sell)\s+(?<country>[A-Za-z]{3})\s+\@\s+(?<price>[0-9]+(?:\.[0-9]{2})?)");
-        private readonly Regex _regexConfig = new Regex(@"^:show\s+(?<show>(bids|offers|countries|owners|market\s+(?<country>[A-Za-z]{3})))");
+        private readonly Regex _regex = new Regex(@"^\:(?<bs>buy|sell)\s+(?<country>[A-Za-z]{3})\s*\@\s*(?<price>[0-9]+(?:\.[0-9]{2})?)");
+        private readonly Regex _regexConfig = new Regex(@"^\:show\s+(?:(?<showc>bids|offers|market)\s+|(?<show>owners|countries))(?(1)(?<country>[A-Za-z]{3})|\.*)");
+        private readonly Regex _regexRemove = new Regex(@"^\:drop\s+(?<team>[A-Za-z]{3})");
+        private readonly Regex _regexOwner = new Regex(@"^\:owner\s+(?<team>[A-Za-z]{3})\s+(?<user>[A-Za-z]+)");
+        private readonly Regex _regexPull = new Regex(@"\:pull\s+(?<bs>buy|sell)\s+(?<country>[A-Za-z]{3})");
 
         private Task ReturnMessage(string message, WebHookHandlerContext context)
         {
@@ -21,59 +26,220 @@ namespace RWCExchange
 
         public override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
         {
-            NameValueCollection nvc;
-            if (context.TryGetData(out nvc))
+            try
             {
-                var user = nvc["user_name"];
-                if (string.IsNullOrEmpty(user))
+                NameValueCollection nvc;
+                if (context.TryGetData(out nvc))
                 {
-                    return ReturnMessage("Invalid User! Sorry....",context);
-                }
-                var text = nvc["text"];
-                var match1 = _regex.Match(text);
-                var match2 = _regexConfig.Match(text);
-                if (!match1.Success && !match2.Success)
-                {
-                    return ReturnMessage("Invalid Syntax! try \"(:buy|:sell) CountryCode @ Price\" to make a buy or sell request.\nYou can also type \":show bids|offers|countries|owners\" to show exchange info.",context);
-                }
-                if (match1.Success)
-                {
-                    var isBuy = match1.Groups["bs"].Value == "buy";
-                    var country = match1.Groups["country"].Value.ToUpper();
-                    var price = match1.Groups["price"].Value;
-                    double priced;
-                    if (!double.TryParse(price, out priced))
+                    var user = nvc["user_name"];
+                    if (string.IsNullOrEmpty(user))
                     {
-                        return ReturnMessage("Invalid price!", context);
+                        return ReturnMessage("Invalid User! Sorry....", context);
                     }
-                    if (!Exchange.Instance.IsValidCountry(country))
+                    var text = nvc["text"];
+                    var tradeMatch = _regex.Match(text);
+                    var showMatch = _regexConfig.Match(text);
+                    var removeMatch = _regexRemove.Match(text);
+                    var ownerMatch = _regexOwner.Match(text);
+                    var pullMatch = _regexPull.Match(text);
+                    if (!tradeMatch.Success && !showMatch.Success && !removeMatch.Success && !ownerMatch.Success && !pullMatch.Success && !text.Trim().Contains(":help"))
                     {
-                        return ReturnMessage("Invalid country code. Try again!", context);
+                        return ReturnMessage("Invalid Syntax! try \":help\"", context);
                     }
-                    var isCurrentOwner = Exchange.Instance.IsCurrentOwner(country, user);
-                    if (isBuy && isCurrentOwner) return ReturnMessage($"{user} you already own {country}! You can't buy it again you fool!",context);
-                    if (!isBuy && !isCurrentOwner)return ReturnMessage($"{user} you dont own {country}. You can't sell something you don't own you fool!",context);
-                    if (isBuy)
+                    if (tradeMatch.Success)
                     {
-                        var trade = Exchange.Instance.AddBid(country,
-                            new Bid {Price = priced, TimeStamp = DateTime.UtcNow, User = user});
-                        if (trade != null)
-                        {
-                            return ReturnMessage($"WOOOO! You traded! {user}, you now own {country}, and you owe {trade.Ask.User} £{trade.Price}. Pay up or I'll send the bailiffs!",context);
-                        }
-                        return ReturnMessage("You're bid has been accepted!",context);
+                        if (!tradeMatch.Groups["bs"].Success || !tradeMatch.Groups["country"].Success ||
+                            !tradeMatch.Groups["price"].Success)
+                            return ReturnMessage("Failed to parse the trade request sorry!", context);
+
+                        var price = tradeMatch.Groups["price"].Value;
+                        double priced;
+                        return !double.TryParse(price, out priced) || priced<=0 ? ReturnMessage("Invalid price!", context) : RunTrade(tradeMatch.Groups["bs"].Value == "buy", tradeMatch.Groups["country"].Value.ToUpper(), user, priced, context);
                     }
-                    else
+                    if (showMatch.Success)
                     {
-                        
+                        return showMatch.Groups["showc"].Success ? RunShow(showMatch.Groups["showc"].Value, context, showMatch.Groups["country"].Value.ToUpper()) : RunShow(showMatch.Groups["show"].Value, context);
                     }
-                    
+                    if (pullMatch.Success)
+                    {
+                        return RunPull(pullMatch.Groups["bs"].Value == "buy", pullMatch.Groups["country"].Value.ToUpper(), user,
+                            context);
+                    }
+                    if (text.Trim().Contains(":help"))
+                    {
+                        return RunHelp(context);
+                    }
+                    if (user != "chrislewis") return ReturnMessage("Permission denied! Don't be so cheeky..", context);
+                    if (removeMatch.Success)
+                    {
+                        return RunDrop(removeMatch.Groups["team"].Value.ToUpper(), context);
+                    }
+                    if (ownerMatch.Success)
+                    {
+                        return RunOwner(ownerMatch.Groups["team"].Value.ToUpper(), ownerMatch.Groups["user"].Value, context);
+                    }
                 }
-                else
-                {
-                    
-                }
+                return Task.FromResult(true);
+            }
+            catch (Exception e)
+            {
+                return ReturnMessage(e.Message,context);
             }
         }
+
+        private Task RunTrade(bool isBuy, string country, string user, double price, WebHookHandlerContext context)
+        {
+            if (!Exchange.Instance.IsValidCountry(country))
+            {
+                return ReturnMessage("Invalid country code. Try again!", context);
+            }
+            if (Exchange.Instance.CountryDropped(country))
+            {
+                return ReturnMessage($"This country is already out of the cup. Come on, keep up! Go {(isBuy?"buy":"sell")} another one.",context);
+            }
+            var isCurrentOwner = Exchange.Instance.IsCurrentOwner(country, user);
+            if (isBuy && isCurrentOwner) return ReturnMessage($"@{user} you already own {country}! You can't buy it again you fool!", context);
+            if (!isBuy && !isCurrentOwner) return ReturnMessage($"@{user} you dont own {country}. You can't sell something you don't own you fool!", context);
+            if (isBuy)
+            {
+                if(!Exchange.Instance.CurrentlyOwned(country))return ReturnMessage($"{country} doesn't yet have an owner, wait until it does before you start to place bids.",context);
+                var trade = Exchange.Instance.AddBid(country,new Bid { Price = price, TimeStamp = DateTime.UtcNow, User = user });
+                if (trade?.Bid != null && trade.Ask!=null)
+                {
+                    return ReturnMessage($"WOOOO! You traded! @{user} you now own {country}, and you owe @{trade.Ask.User} *£{trade.Price}*. Pay up or I'll send the bailiffs!", context);
+                }
+                if (trade != null && trade.Update)
+                {
+                    return ReturnMessage($"@{user} you're buy request has been updated for {country}. Let's hope we can get a matching sale price.",context);
+                }
+                return ReturnMessage("You're bid has been accepted, but it's not triggered a trade yet. Fingers crossed for you!", context);
+            }
+            else
+            {
+                var trade = Exchange.Instance.AddAsk(country, new Ask{ Price = price, TimeStamp = DateTime.UtcNow, User = user });
+                if (trade?.Bid != null && trade.Ask != null)
+                {
+                    return ReturnMessage($"WOOOO! You traded! @{trade.Bid.User} you now own {country}, and you owe @{user} *£{trade.Price}*. Pay up or I'll send the bailiffs!", context);
+                }
+                if (trade != null && trade.Update)
+                {
+                    return ReturnMessage($"@{user} you're sell request has been update for {country}. Let's wait for a buyer!",context);
+                }
+                return ReturnMessage("You're sell offer has been accepted but it's not triggered a trade yet. Fingers crossed for you!", context);
+            }
+        }
+
+        private Task RunShow(string showWhat, WebHookHandlerContext context, string country = null)
+        {
+            if (country != null)
+            {
+                if (!Exchange.Instance.IsValidCountry(country))
+                {
+                    return ReturnMessage("Invalid country code. Try again!", context);
+                }
+                if (Exchange.Instance.CountryDropped(country))
+                {
+                    return ReturnMessage("This country is already out of the cup. Come on, keep up!", context);
+                }
+                switch (showWhat)
+                {
+                    case "bids":
+                        return ReturnMessage($"*Bids for {country}:*\n" + string.Join("\n", Exchange.Instance.GetBids(country).Select(i => $"Price: *{i.Price}*, User: @{i.User}")), context);
+                    case "offers":
+                        return ReturnMessage($"*Offers for {country}:*\n" + string.Join("\n", Exchange.Instance.GetAsks(country).Select(i => $"Price: *{i.Price}*, User: @{i.User}")), context);
+                    case "market":
+                        var bestBid = Exchange.Instance.GetBestBid(country);
+                        var bestAsl = Exchange.Instance.GetBestAsk(country);
+                        var bidString = bestBid == null ? "_No Bids_" : $"Bid Price: *{bestBid.Price}* @{bestBid.User}";
+                        var askString = bestAsl == null ? "_No asks_" : $"Ask Price: *{bestAsl.Price}* @{bestAsl.User}";
+                        return ReturnMessage($"*Best Market for {country}:*\n" + bidString + "     " + askString, context);
+                }
+            }
+            var owners = Exchange.Instance.GetOwners();
+            switch (showWhat)
+            {
+                case "owners":
+                    return ReturnMessage("*Current Owners:*\n"+string.Join("\n",owners.Select(i=>$"{i.Key}" +"     "+ (i.Value==string.Empty?"_none_":"@"+i.Value))),context);
+                case "countries":
+                    return ReturnMessage("*Countries Left in the Tournament:*\n" + string.Join("\n", owners.Select(i => $"{i.Key}")),context);
+            }
+            return Task.FromResult(true);
+
+        }
+
+        private Task RunDrop(string country, WebHookHandlerContext context)
+        {
+            if (!Exchange.Instance.IsValidCountry(country))
+            {
+                return ReturnMessage("Invalid country code. Try again!", context);
+            }
+            if (Exchange.Instance.CountryDropped(country))
+            {
+                return ReturnMessage("This country is already out of the cup. Come on, keep up!", context);
+            }
+            var user = Exchange.Instance.DropTeam(country);
+            return ReturnMessage($"{country} has been dropped from the tournament. Sorry @{user}",context);
+        }
+
+        private Task RunOwner(string country, string owner, WebHookHandlerContext context)
+        {
+            if (!Exchange.Instance.IsValidCountry(country))
+            {
+                return ReturnMessage("Invalid country code. Try again!", context);
+            }
+            if (Exchange.Instance.CountryDropped(country))
+            {
+                return ReturnMessage("This country is already out of the cup. Come on, keep up!", context);
+            }
+            if (Exchange.Instance.SetOwner(country, owner))
+            {
+                ReturnMessage($"Congrats @{owner} You now own {country}.", context);
+            }
+            return Task.FromResult(true);
+        }
+
+        private Task RunPull(bool isBid, string country, string user, WebHookHandlerContext context)
+        {
+            if (!Exchange.Instance.IsValidCountry(country))
+            {
+                return ReturnMessage("Invalid country code. Try again!", context);
+            }
+            if (Exchange.Instance.CountryDropped(country))
+            {
+                return ReturnMessage("This country is already out of the cup. Come on, keep up!", context);
+            }
+            if (!isBid && !Exchange.Instance.IsCurrentOwner(country, user))
+            {
+                return ReturnMessage($"You aren't the current owner of {country}, so you have no offers in the market.",context);
+            }
+            if (isBid)
+            {
+                if (Exchange.Instance.PullBid(country, user))
+                    return ReturnMessage($"Thanks @{user} You're bid for {country} has been dropped from the market.", context);
+                ReturnMessage($"{user} - Pull failed. Do you even have a bid in the market?", context);
+            }
+            if(Exchange.Instance.PullAsk(country,user))return ReturnMessage($"Thanks @{user} You're offer to sell {country} has been dropped from the market.",context);
+            return ReturnMessage($"@{user} - Pull failed. Do you even have a sell offer in the market?", context);
+        }
+
+        private Task RunHelp(WebHookHandlerContext context)
+        {
+            var sb = new StringBuilder("*Help:*\n");
+            sb.AppendLine(
+                "To make a bid or request a sale - \":<buy|sell> <countryCode> @ <price>\" - Note: you can only buy a team you don't own and you can only sell one that you do own.\n" +
+                "                                                                                  The exchange will make a trade if you're price crossed the market best bid/ask. \n" +
+                "                                                                                  The price of the trade will be the price you enter, so check first!\n" +
+                "                                                                                  If 2 or more people have bids in at the same price then its first come first serve.\n" +
+                "                                                                                  If you already have a bid or offer in the market then this command will update the price and time on that bid or offer.");
+            sb.AppendLine("To show current best prices for country - \":show market <countryCode>\"");
+            sb.AppendLine("To show all bids/offers on country - \":show <bids|offers> <countryCode>\"");
+            sb.AppendLine("To show current ownership - \":show owners\"");
+            sb.AppendLine("To show all remaining teams - \":show countries\"");
+            sb.AppendLine("To remove you're current bid or ask - \":pull <buy|sell> <countryCode>\"");
+            return ReturnMessage(sb.ToString(),context);
+        }
+
+
+
     }
 }
